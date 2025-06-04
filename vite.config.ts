2792140c4c86369e869c6b78f787b5f868948195
ter4744411 <73571121+ter4744411@users.noreply.github.com>
2025-06-04 15:21:34 +0700

    rename repo

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/auto-delete-todolist/',
+  base:'/auto-delete-todolist.github.io/',
   plugins: [
     tailwindcss(), react()],
 })
